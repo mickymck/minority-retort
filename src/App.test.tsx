c84a4f8db705d6c9366b0a2e6cc9a7c5acd8a793
Micky McKeon <mickymckeon@gmail.com>
2024-02-25 15:29:01 -0500

    finished unit testing App (for now) + mock report + useParams + useReport

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
+// I need to mock axios first so we don't get an error trying to import the outside module
 jest.mock('axios', () => {
 	return {
 		create: jest.fn(() => jest.fn()),
 		get: jest.fn(() => Promise.resolve({ data: { results: [] } })),
 	};
 });
+
+// Still need to import axios and work with it before other imports, I think
 import axios from 'axios';
 import { fetchGenreMock } from './__mocks__/fetchGenreMock';
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -20,7 +23,25 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
+
+import { ReportProvider } from './services/ReportProvider';
+import { reportMock } from './__mocks__/reportMock';
+
 import App from './App';
+
+// mock the useParams hook with the Dune id
+jest.mock('react-router-dom', () => ({
+	...jest.requireActual('react-router-dom'),
+	useParams: jest.fn().mockReturnValue({ id: '693134' }),
+}));
+
+// mock the useReport hook with the reportMock data
+jest.mock('./services/ReportProvider', () => ({
+	...jest.requireActual('./services/ReportProvider'),
+	useReport: jest.fn(() => ({
+		report: reportMock,
+	})),
+}));
 
 describe('App component', () => {
 	test('FlickList renders at the root path', async () => {
@@ -33,6 +54,22 @@ describe('App component', () => {
 		});
 		await waitFor(() => {
 			expect(screen.getByText(/Shin Spider-Man/i)).toBeInTheDocument();
+		});
+	});
+
+	test('FlickDetail renders the proper movie details', async () => {
+		await act(async () => {
+			render(
+				<MemoryRouter initialEntries={['/film/693134']}>
+					<ReportProvider>
+						<App />
+					</ReportProvider>
+				</MemoryRouter>
+			);
+		});
+		await waitFor(() => {
+			expect(screen.getByText(/Dune: Part Two/i)).toBeInTheDocument();
+			expect(screen.getByText(/Back/i)).toBeInTheDocument();
 		});
 	});
 });
---
+++ src/__mocks__/reportMock.ts
@@ -0,0 +1,23 @@
+export const reportMock = [
+	{
+		id: 634492,
+		title: 'Madame Web',
+		releaseDate: '2024-02-14',
+		popularity: 441.913,
+		imageUrl: '/rULWuutDcN5NvtiZi4FRPzRYWSh.jpg',
+	},
+	{
+		id: 693134,
+		title: 'Dune: Part Two',
+		releaseDate: '2024-02-27',
+		popularity: 484.985,
+		imageUrl: '/8b8R8l88Qje9dn9OE8PY05Nxl1X.jpg',
+	},
+	{
+		id: 1241534,
+		title: 'Shin Spider-Man',
+		releaseDate: '2024-02-04',
+		popularity: 2.611,
+		imageUrl: '/h2ncfhfr3bwdtZ2CVtC1ky18owL.jpg',
+	},
+];
